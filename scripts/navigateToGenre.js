8a18c1938e55760a9ecda7f564caced5a9c9faea
nthsemboshilowa <2464956@students.wits.ac.za>
2025-11-08 23:20:45 +0200

    files location fix. pages were not jumping to the correct folders

--- scripts/navigateToGenre.js
+++ scripts/navigateToGenre.js
@@ -1,19 +1,19 @@
 function goToHome() {
-    window.location.href = '/index.html'; 
+    window.location.href = '../home/index.html'; 
 }
 
 function goToFilms() {
-    window.location.href = '/films/index.html'; 
+    window.location.href = '../films/index.html'; 
 }
 
 function goToGenre() {
-    window.location.href = '/genres/index.html'; 
+    window.location.href = '../genres/index.html'; 
 }
 
 function goToWatchlist() {
-    window.location.href = '/watchlist/index.html'; 
+    window.location.href = '../watchlist/index.html'; 
 }
 
 function goToFavourites() {
-    window.location.href = '/favourites/index.html'; 
+    window.location.href = '../favourites/index.html'; 
 }
