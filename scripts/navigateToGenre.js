21feeb4b95ca848484a263cd8b84decf5cf24a3c
nthsemboshilowa <2464956@students.wits.ac.za>
2025-11-08 22:39:56 +0200

    Website with working Sign-up page. Navigation and other formatting fixed.
    Note to self: Remember to change navbar "home" location.

---
+++ scripts/navigateToGenre.js
@@ -0,0 +1,19 @@
+function goToHome() {
+    window.location.href = '/index.html'; 
+}
+
+function goToFilms() {
+    window.location.href = '/films/index.html'; 
+}
+
+function goToGenre() {
+    window.location.href = '/genres/index.html'; 
+}
+
+function goToWatchlist() {
+    window.location.href = '/watchlist/index.html'; 
+}
+
+function goToFavourites() {
+    window.location.href = '/favourites/index.html'; 
+}
